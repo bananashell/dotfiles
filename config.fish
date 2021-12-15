772ea6b4d7016a927c440f922735e02752357cda
Joakim Jäderberg <15868581+bananashell@users.noreply.github.com>
2021-12-15 14:03:26 +0100

    .

--- config.fish
+++ config.fish
@@ -1,11 +1,15 @@
 set NODE_OPTIONS --max-old-space-size=3072
 
 function ll
-    exa --icons --oneline --all --git-ignore --group-directories-first
+    exa --icons --all --git-ignore --group-directories-first $argv
 end
 
 function lt
-    exa --icons --tree --level=2 --all --git-ignore --group-directories-first
+    exa --icons --tree --level=2 --all --git-ignore --group-directories-first $argv
+end
+
+function ld
+    exa --icons --all --git-ignore --group-directories-first --only-dirs $argv
 end
 
 function gs
