f052e3dd4aa1ae6fff5ec96eff8583ff55692b48
Joakim Jäderberg <15868581+bananashell@users.noreply.github.com>
2021-12-15 11:41:02 +0100

    added vscode settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,11 +6,11 @@ git clone git@github.com/bananashell/dotfiles.git ~/.dotfiles
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
-ln -s ~/.dotfiles/VSCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -s ~/.dotfiles/VSCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -s ~/.dotfiles/VSCode/snippets/ ~/Library/Application\ Support/Code/User
-
 #install homebrew
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 
 cd ~/.dotfiles && brew bundle
+
+ln -s ~/.dotfiles/VSCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/.dotfiles/VSCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/.dotfiles/VSCode/snippets/ ~/Library/Application\ Support/Code/User
